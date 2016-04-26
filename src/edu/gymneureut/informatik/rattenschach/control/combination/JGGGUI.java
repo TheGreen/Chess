085ea2d557e776c317bfb4084ce678af9222adb8
Jan Christian Grünhage <mail@janchristiangruenhage.de>
2016-04-26 16:30:40 +0200

    JGGGUI Implementation Implemented Methods of Controller and Observer Interfaces

--- src/edu/gymneureut/informatik/rattenschach/control/combination/JGGGUI.java
+++ src/edu/gymneureut/informatik/rattenschach/control/combination/JGGGUI.java
@@ -3,6 +3,13 @@ package edu.gymneureut.informatik.rattenschach.control.combination;
 import ch.aplu.jgamegrid.GameGrid;
 import edu.gymneureut.informatik.rattenschach.control.controller.Controller;
 import edu.gymneureut.informatik.rattenschach.control.observer.Observer;
+import edu.gymneureut.informatik.rattenschach.model.Field;
+import edu.gymneureut.informatik.rattenschach.model.Game;
+import edu.gymneureut.informatik.rattenschach.model.figures.Figure;
+import edu.gymneureut.informatik.rattenschach.model.turns.Turn;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * The <tt>JGameGridGraphicalUserInterface</tt> class.
@@ -10,5 +17,40 @@ import edu.gymneureut.informatik.rattenschach.control.observer.Observer;
  * @author Jan Christian Gruenhage, Alex Klug
  * @version 0.1
  */
-public class JGGGUI implements Controller, Observer, GameGrid {
+public class JGGGUI extends GameGrid implements Controller, Observer {
+
+    @Override
+    public Turn pickMove(Map<Field, Figure> field, List<Turn> turns) {
+        return null;
+    }
+
+    @Override
+    public void hasWon() {
+
+    }
+
+    @Override
+    public void hasLost() {
+
+    }
+
+    @Override
+    public void isStalemate() {
+
+    }
+
+    @Override
+    public void isDraw() {
+
+    }
+
+    @Override
+    public void startGame(Game game) {
+
+    }
+
+    @Override
+    public void nextTurn(Turn turn) {
+
+    }
 }
