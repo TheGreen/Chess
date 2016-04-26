15130e36766d54e7cf79d6ad45bd8d3b9d33197e
Jan Christian Grünhage <mail@janchristiangruenhage.de>
2016-04-26 12:55:13 +0200

    Added the JGameGrid Library, created the JGameGridGraphicalUserInterface class.

---
+++ src/edu/gymneureut/informatik/rattenschach/control/combination/JGGGUI.java
@@ -0,0 +1,14 @@
+package edu.gymneureut.informatik.rattenschach.control.combination;
+
+import ch.aplu.jgamegrid.GameGrid;
+import edu.gymneureut.informatik.rattenschach.control.controller.Controller;
+import edu.gymneureut.informatik.rattenschach.control.observer.Observer;
+
+/**
+ * The <tt>JGameGridGraphicalUserInterface</tt> class.
+ *
+ * @author Jan Christian Gruenhage, Alex Klug
+ * @version 0.1
+ */
+public class JGGGUI implements Controller, Observer, GameGrid {
+}
