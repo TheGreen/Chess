f2a2dbd8187eb097fe2daf30d8f536000705701c
Jan Christian Grünhage <mail@janchristiangruenhage.de>
2016-04-29 13:18:13 +0200

    Shortened the move time in JGGGUI

--- src/edu/gymneureut/informatik/rattenschach/control/combination/JGGGUI.java
+++ src/edu/gymneureut/informatik/rattenschach/control/combination/JGGGUI.java
@@ -151,7 +151,7 @@ public class JGGGUI extends GameGrid implements Controller, Observer {
         updateFieldView(turn);
         //updateFieldView();
         try {
-            Thread.sleep(500);
+            Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
