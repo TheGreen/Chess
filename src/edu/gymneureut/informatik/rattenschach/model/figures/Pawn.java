c6963d5a625b50ca7ac67749aa70e804e075835d
Jan Christian Grünhage <mail@janchristiangruenhage.de>
2016-05-11 11:57:15 +0200

    Bugfixes and small additions:  - Pawn could capture friendly figures.  - toString for Notifications  - TerminalObserverSlim as Logger for JGGGUI Test

--- src/edu/gymneureut/informatik/rattenschach/model/figures/Pawn.java
+++ src/edu/gymneureut/informatik/rattenschach/model/figures/Pawn.java
@@ -22,6 +22,18 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Jan Christian Grünhage; Alex Klug
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package edu.gymneureut.informatik.rattenschach.model.figures;
 
 import edu.gymneureut.informatik.rattenschach.model.Field;
@@ -75,7 +87,8 @@ public class Pawn extends Figure implements Cloneable {
 
         if (position.getFile() > 1) {
             destination = forwardLeft(position);
-            if (field.get(destination) != EMPTY) {
+            if (field.get(destination) != EMPTY
+                    && field.get(destination).getOwner() != this.getOwner()) {
                 getPossibleMoves(destination, true, moves);
             }
         }
@@ -84,7 +97,8 @@ public class Pawn extends Figure implements Cloneable {
 
         if (position.getFile() < 8) {
             destination = forwardRight(position);
-            if (field.get(destination) != EMPTY) {
+            if (field.get(destination) != EMPTY
+                    && field.get(destination).getOwner() != this.getOwner()) {
                 getPossibleMoves(destination, true, moves);
             }
         }
@@ -105,7 +119,6 @@ public class Pawn extends Figure implements Cloneable {
             moves.add(new Promotion(this.getOwner(), this, position, destination, captures, field.get(destination),
                     new Bishop(this.owner, position, field)));
         }
-        return;
     }
 
     private Field forwardStraight(Field position) {
